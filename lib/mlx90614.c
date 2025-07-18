9d6b327cf4ef6e4bee47f8be0fe9fa993ddf7ca0
XunMarahiro <280160112@qq>
2025-07-18 17:34:56 +0800

    add mlx90614 driver

---
+++ lib/mlx90614.c
@@ -0,0 +1,10 @@
+#include "mlx90614.h"
+void write_8bit(int data)
+{
+}
+int read_8bit()
+{
+    int value;
+    return value;
+}
+void
---
+++ lib/mlx90614.h
@@ -0,0 +1,7 @@
+#ifndef _mlx90614_h_
+#define _mlx90614_h_
+#include "../drive.h"
+
+#define mlx_address 0x00
+
+#endif
